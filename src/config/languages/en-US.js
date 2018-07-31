9b809cae1fd143452ec7feaeefbb126a9a73f382
sleeplesseditor <josh@bryson.me.uk>
2018-07-31 19:50:34 +0100

    Language config setup

---
+++ src/config/languages/en-US.js
@@ -0,0 +1,26 @@
+export default en_US = {
+    languageKey: 'en_US',
+    home: {
+        searchByTag: 'Search by tag',
+        addImageOptions: [
+            'Gallery',
+            'Camera',
+            'Cancel'
+        ]
+    },
+    feed: {
+        noImagesToDisplay: 'No Images to Display',
+    },
+    imageEditor: {
+        tagsPlaceholder: 'Tags (at least one required)',
+        captionPlaceholder: 'Caption (optional)',
+        includeLocation: 'Include location'
+    },
+    settings: {
+        languagesOptions: [
+            'English',
+            'Spanish',
+            'Cancel'
+        ]
+    }
+};
---
+++ src/config/languages/es-CR.js
@@ -0,0 +1,26 @@
+export default es_CR = {
+    languageKey: 'es-CR',
+    home: {
+        searchByTag: 'Buscar por tag',
+        addImageOptions: [
+            'Galería',
+            'Cámara',
+            'Cancelar'
+        ]
+    },
+    feed: {
+        noImagesToDisplay: 'No hay imágenes para mostrar'
+    },
+    imageEditor: {
+        tagsPlaceholder: 'Tags (al menos uno requerido)',
+        captionPlaceholder: 'Título (opcional)',
+        includeLocation: 'Incluir locación'
+    },
+    settings: {
+        languagesOptions: [
+            'Inglés',
+            'Español',
+            'Cancelar'
+        ]
+    }
+}
